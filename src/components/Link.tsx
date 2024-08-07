96a8af7bfb5d15cfd22ba783907c3b2a30b307b5
Dragan Aleksic <aleksicdragan574@gmail.com>
2024-08-07 14:06:54 +0200

    Update: re-render all Route components on click event

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -1,3 +1,5 @@
+import { stateSetters } from "../lib/utils";
+
 type LinkProps = {
     to: string,
     replace?: boolean,
@@ -12,10 +14,16 @@ export function Link({ to, replace, children }: LinkProps) {
 
     const historyPush = (path: string) => {
         history.pushState({}, '', path)
+
+        // Trigger a re-render of all Route components
+        stateSetters.forEach(stateSetter => stateSetter({}))
     }
 
     const historyReplace = (path: string) => {
         history.replaceState({}, '', path)
+        
+        // Trigger a re-render of all Route components
+        stateSetters.forEach(stateSetter => stateSetter({}))
     }
 
     return <a href={to} onClick={handleClick}>{children}</a>
