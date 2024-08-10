057f86394249b6f1ec990a82a93f8f85893d632a
Dragan Aleksic <aleksicdragan574@gmail.com>
2024-08-10 12:05:40 +0200

    Add redirect component

---
+++ src/components/Redirect.tsx
@@ -0,0 +1,27 @@
+import { useEffect } from "react";
+import { stateSetters } from "../lib/utils";
+
+type RedirectProps = {
+    to: string;
+    push?: boolean;
+}
+
+export function Redirect({ to, push = false }: RedirectProps) {
+    useEffect(() => {
+        push ? historyPush(to) : historyReplace(to)
+    }, [])
+
+    const historyPush = (path: string) => {
+        history.pushState({}, '', path)
+
+        // Trigger a re-render of all Route components
+        stateSetters.forEach(stateSetter => stateSetter({}))
+    }
+
+    const historyReplace = (path: string) => {
+        history.replaceState({}, '', path)
+        
+        // Trigger a re-render of all Route components
+        stateSetters.forEach(stateSetter => stateSetter({}))
+    }
+}
